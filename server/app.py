e55f8959232436a61281c8f0e936f568e266d20e
Yisak Metaferiya <yisakmetaferiya1@gmail.com>
2025-11-26 04:50:33 +0400

    backing support json file generation

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+from model.gemini_requests import Gemini
 
 app = Flask(__name__)
 
@@ -7,6 +8,24 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
+@app.route('/content', methods=['GET'])
+def get_content(level=1):
+    nums = 40
+    prompts = "animal story, in json format of {\"content\": \"actual content\"} the key content is a must"
+    match level:
+        case 1:
+            prompts += " with exactly 40 words length"
+            gemini = Gemini(prompts)
+            response = gemini.get_response()
+            return response
+        case 2:
+            prompts += " with exactly 80 words length"
+            gemini = Gemini(prompts)
+            response = gemini.get_response()
+            return response
+
+    return jsonify("{'content': \"actual content\"}")
 
 
 if __name__ == '__main__':
--- server/model/gemini_requests.py
+++ server/model/gemini_requests.py
@@ -1,8 +1,25 @@
+import google.generativeai as gemai
+from dotenv import load_dotenv
+import os
 
+from flask import jsonify
+
+load_dotenv()
+
+
+gemai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = gemai.GenerativeModel(os.getenv("GEMINI_MODEL"))
 
 class Gemini:
     def __init__(self, prompt):
         self.prompt = prompt
 
     def get_response(self):
-        pass
+        try:
+            response = model.generate_content(self.prompt)
+            return jsonify(response)
+        except Exception as e:
+            return jsonify({"error": "Something went wrong."}, error=e)
+        return None
+
+
